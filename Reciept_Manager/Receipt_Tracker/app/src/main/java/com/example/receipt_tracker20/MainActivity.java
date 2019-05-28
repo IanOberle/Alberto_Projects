db0f17101fc0d6c994ae28d55d2868eef2d25050
IanOberle <ioberle20@gmail.com>
2019-05-28 16:38:23 -0500

    Update

---
+++ Reciept_Manager/Receipt_Tracker/app/src/main/java/com/example/receipt_tracker20/MainActivity.java
@@ -0,0 +1,425 @@
+package com.example.receipt_tracker20;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.ProgressListener;
+import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.session.AppKeyPair;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button2;
+    Button button3;
+    Button button5;
+    TextView textAddress;
+    static DropboxAPI<AndroidAuthSession> dropboxAPI;
+    private static final String APP_KEY = "bluik4guz6zjprn";
+    private static final String APP_SECRET = "tcmo82tgt089c6y";
+    private static final String ACCESSTOKEN = "yRQkBJpRSOAAAAAAAAAATXckbsl-XOrMLi9kw-M4SutiS7BwnptM-B5-ZQBswKaH";
+    private static final String Google_API_KEY = "AIzaSyBwYov9vt_XRJyMb8-y-Ue3top_npkTNx0";
+    LocationManager mLocationManager;
+    double lat, lng;
+    String address;
+    String stamp;
+    final int MY_PERMISSION_REQUEST_CODE = 7171;
+    public static int IfFiller = 1;
+    static final int UploadFromSelectApp = 9501;
+    static final int UploadFromFilemanager = 9502;
+    public static String DropboxUploadPathFrom = "";
+    public static String DropboxUploadName = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button5 = findViewById(R.id.button5);
+        textAddress = findViewById(R.id.textView2);
+        AndroidAuthSession session = buildSession();
+        dropboxAPI = new DropboxAPI<>(session);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]
+                            {Manifest.permission.ACCESS_COARSE_LOCATION,
+                                    Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSION_REQUEST_CODE);
+        }
+
+        long time = System.currentTimeMillis();
+        Timestamp timestamp = new Timestamp(time);
+        stamp = timestamp.toString();
+
+        button5.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                if (isPermissionGranted()) {
+                    call_action();
+                }
+            }
+        });
+
+    }
+
+    public void onClickBut2 (View v)
+    {
+        if(isPermissionGranted())
+            call_action();
+        UploadToDropboxFromSelectedApp(address + lat + lng + stamp);
+    }
+    public void onClickBut3 (View v)
+    {
+        if(isPermissionGranted())
+            call_action();
+        UploadToDropboxFromFilemanager(address + lat + lng + stamp);
+    }
+
+    private Location getLastKnownLocation()
+    {
+        mLocationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+        List<String> providers = mLocationManager.getProviders(true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return null;
+        }
+        Location bestLocation = null;
+        for (String provider : providers)
+        {
+            Location l = mLocationManager.getLastKnownLocation(provider);
+            if (l == null)
+                continue;
+            if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy())
+                bestLocation = l;
+        }
+        return bestLocation;
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        if (requestCode == MY_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                IfFiller = 2;
+        }
+    }
+
+    public  boolean isPermissionGranted()
+    {
+        if (Build.VERSION.SDK_INT >= 23)
+        {
+            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED &&
+                    checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED)
+            {
+                Log.v("TAG","Permission is granted");
+                return true;
+            }
+            else
+            {
+                Log.v("TAG","Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+                return false;
+            }
+        }
+        else
+        {
+            Log.v("TAG","Permission is granted");
+            return true;
+        }
+    }
+
+    public void call_action()
+    {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+            return;
+        Location myLocation = null;
+        if (isPermissionGranted())
+        {
+            myLocation = getLastKnownLocation();
+        }
+        try
+        {
+
+            if (myLocation == null)
+                Log.v("TAG","Invalid or no location provided");
+            else
+            {
+                lat = myLocation.getLatitude();
+                lng = myLocation.getLongitude();
+            }
+
+        }
+        catch (Exception ex)
+        {
+            lat = 39.0233;
+            lng = -94.8279;
+        }
+        if (myLocation != null)
+            new GetAddress().execute(String.format(Locale.ENGLISH,"%.4f,%.4f",lat,lng));
+
+
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private class GetAddress extends AsyncTask<String,Void,String>
+    {
+        ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("Please wait...");
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.show();
+        }
+
+        protected String doInBackground(String... strings)
+        {
+            try
+            {
+                double lat = Double.parseDouble(strings[0].split(",")[0]);
+                double lng = Double.parseDouble(strings[0].split(",")[1]);
+                String response;
+                HTTPDataHandler http = new HTTPDataHandler();
+                String url = String.format(Locale.ENGLISH,"https://maps.googleapis.com/maps/api/geocode/json?latlng=%.4f,%.4f&key=%s", lat , lng, Google_API_KEY);
+                response = http.GetHTTPData(url);
+                return response;
+            }
+            catch (Exception ex) {return  null;}
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            try
+            {
+                JSONObject jsonObject = new JSONObject(s);
+                address = ((JSONArray)jsonObject.get("results")).getJSONObject(0).get("formatted_address").toString();
+                textAddress.setText(address);
+            }
+            catch (JSONException e) {e.printStackTrace();}
+
+            if (dialog.isShowing())
+                dialog.dismiss();
+        }
+    }
+
+    private AndroidAuthSession buildSession()
+    {
+        AppKeyPair appKeyPair = new AppKeyPair(APP_KEY, APP_SECRET);
+        AndroidAuthSession session = new AndroidAuthSession(appKeyPair);
+        session.setOAuth2AccessToken(ACCESSTOKEN);
+        return session;
+    }
+
+    private void UploadToDropboxFromPath (String uploadPathFrom, String uploadPathTo)
+    {
+        Toast.makeText(getApplicationContext(), "Upload file ...", Toast.LENGTH_SHORT).show();
+        final String uploadPathF = uploadPathFrom;
+        final String uploadPathT = uploadPathTo;
+        Thread th = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                File tmpFile = null;
+                try
+                {
+                    tmpFile = new File(uploadPathF);
+                }
+                catch (Exception e) {e.printStackTrace();}
+                FileInputStream fis = null;
+                try
+                {
+                    fis = new FileInputStream(tmpFile);
+                }
+                catch (FileNotFoundException e) {e.printStackTrace();}
+                try
+                {
+                    if (tmpFile == null)
+                        return;
+                    dropboxAPI.putFileOverwrite(uploadPathT, fis, tmpFile.length(), null);
+                }
+                catch (Exception e) {return;}
+                getMain().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "File successfully uploaded.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        th.start();
+    }
+
+    private void UploadToDropboxFromSelectedApp (String uploadName)
+    {
+        DropboxUploadName = uploadName;
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        startActivityForResult(Intent.createChooser(intent, "Upload from ..."), UploadFromSelectApp);
+    }
+
+    private void UploadToDropboxFromFilemanager (String uploadName)
+    {
+        DropboxUploadName = uploadName;
+        Intent intent = new Intent("com.sec.android.app.myfiles.PICK_DATA");
+        intent.putExtra("CONTENT_TYPE", "*/*");
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        startActivityForResult(intent, UploadFromFilemanager);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent)
+    {
+        if (requestCode == UploadFromFilemanager)
+        {
+            final Uri currFileURI = intent.getData();
+            if (currFileURI == null)
+                return;
+            final String pathFrom = currFileURI.getPath();
+            Toast.makeText(getApplicationContext(), "Upload file ...", Toast.LENGTH_SHORT).show();
+            Thread th = new Thread(new Runnable()
+            {
+                public void run()
+                {
+                    getMain().runOnUiThread(new Runnable()
+                    {
+                        @Override
+                        public void run()
+                        {
+                            if(pathFrom == null)
+                                return;
+                            UploadToDropboxFromPath(pathFrom,DropboxUploadName + pathFrom.substring(pathFrom.lastIndexOf('.')));
+                            Toast.makeText(getApplicationContext(), "File successfully uploaded.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            });
+            th.start();
+        }
+        if (requestCode == UploadFromSelectApp)
+        {
+            Toast.makeText(getApplicationContext(), "Upload file ...", Toast.LENGTH_SHORT).show();
+            final Uri uri = intent.getData();
+
+            DropboxUploadPathFrom = getPath(getApplicationContext(), uri);
+            if(DropboxUploadPathFrom == null) {
+                if (uri == null)
+                    return;
+                DropboxUploadPathFrom = uri.getPath();
+            }
+            Thread th = new Thread(new Runnable(){
+                public void run() {
+                    try
+                    {
+                        final File file = new File(DropboxUploadPathFrom);
+                        if (uri == null)
+                            return;
+                        InputStream inputStream = getContentResolver().openInputStream(uri);
+
+                        dropboxAPI.putFile(DropboxUploadName + file.getName().substring(file.getName().lastIndexOf(".")),
+                                inputStream, file.length(), null, new ProgressListener(){
+                            @Override
+                            public long progressInterval() {return 100;}
+                            @Override
+                            public void onProgress(long arg0, long arg1){}
+                        });
+                        getMain().runOnUiThread(new Runnable()
+                        {
+                            @Override
+                            public void run()
+                            {
+                                Toast.makeText(getApplicationContext(), "File successfully uploaded.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    } catch (Exception e) {e.printStackTrace();}
+                }
+            });
+            th.start();
+        }
+        super.onActivityResult(requestCode, resultCode, intent);
+    }
+
+    public String getPath(Context context, Uri contentUri) {
+        Cursor cursor;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA, MediaStore.Video.Media.DATA, MediaStore.Audio.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            if (cursor == null)
+                return null;
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            String s = cursor.getString(column_index);
+            if(s!=null) {
+                cursor.close();
+                return s;
+            }
+        }
+        catch(Exception e){return null;}
+        try {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
+            cursor.moveToFirst();
+            String s = cursor.getString(column_index);
+            if(s!=null) {
+                cursor.close();
+                return s;
+            }
+        }
+        catch(Exception e){ return null;}
+        try {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA);
+            cursor.moveToFirst();
+            String s = cursor.getString(column_index);
+            cursor.close();
+            return s;
+        }
+        finally {
+            cursor.close();
+        }
+    }
+
+    public MainActivity getMain()
+    {
+        return this;
+    }
+}
